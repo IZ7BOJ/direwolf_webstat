886d98b45a02769b4f33d8b903bbaa6cb596476d
Alfredo Vania - IZ7BOJ <38103662+IZ7BOJ@users.noreply.github.com>
2021-12-31 00:43:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include 'functions.php';
 logexists();
 
 if((!isset($_SESSION['if'])) or (isset($_SESSION['if']) and ($_SESSION['if'] == ""))) { //if interface was not selected
-	if(($static_if == 1) && ($static_if_index != "")) { // if static interface is declared in config.php
+	if(($static_if == 1) && isset($static_if_index)) { // if static interface is declared in config.php
 		session_start();
 		$_SESSION['if'] = $static_if_index; //open session with static interface declared in config.hp
 		header('Refresh: 0; url=summary.php');
