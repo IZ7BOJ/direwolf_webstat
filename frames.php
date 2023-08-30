04ab22ee2e0d523332fd52d583f1806ce1d9bdf8
Alfredo Vania - IZ7BOJ <38103662+IZ7BOJ@users.noreply.github.com>
2023-08-30 22:41:41 +0200

    added handling of unique log file

--- frames.php
+++ frames.php
@@ -79,7 +79,7 @@ table, th, td {
 	<tbody>
 		<tr align="center">
 		<?php
-		$logfile = file($logpath.gmdate("Y-m-d").".log"); //read log file
+		$logfile = file($log); //read log file
 		$header = str_getcsv($logfile[0]); // take header from first row
 		echo '<th bgcolor="#ffd700"><b><font color="blue">Date</font></b></th>';
 		echo '<th bgcolor="#ffd700"><b><font color="blue">Time(Z)</font></b></th>';
