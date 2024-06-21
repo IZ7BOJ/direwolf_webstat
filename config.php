b68a910f9443727beb43c39d357c073610c92b49
Adrian Grzeca <adriangrzeca@outlook.com>
2024-06-21 16:47:39 +0200

    Added timezone handling, added service status detection via systemd and fixed version detection

--- config.php
+++ config.php
@@ -3,23 +3,26 @@
 Configuration file for Direwolf SimpleWebstat.
 ********************************************************/
 
+//timezone
+$timezone = "Europe/Warsaw";
+
 $version = "0.1beta";
-$logpath = "/var/log/direwolf/"; //path of the direwolf log directory, without file name
+$logpath = "/opt/direwolf/log/"; //path of the direwolf log directory, without file name
 $logname = ""; //log file name. If empty, the standard Y-m-d.log file name (generated every day by direwolf) will be used.
 
 //interfaces index declared in direwolf conf file. Indexes separated by commas
-$interfaces=array(0,1);
+$interfaces=array(0);
 
 //interface descriptions. Insert strings between "" and separated by commas
-$intdesc=array("144.800  VHF Port","432.500 UHF Port");
+$intdesc=array("144.800  Warszawa East");
 
 //static_if skips interface choice every time
 $static_if = 1; //1 to enable static interface 
 $static_if_index = 0; //interface index when static_if is enabled
 
 //station posistion data for calculating distance from received station
-$stationlat = 41.248027; //station latitude in decimal degrees
-$stationlon = 16.421246;  //station longtitude in decimal degrees
+$stationlat = 52.00000; //station latitude in decimal degrees
+$stationlon = 21.00000;  //station longtitude in decimal degrees
 
 //logo path,with file name, shown on the top of the page
 $logourl="direwolf.png";
